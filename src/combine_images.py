5e9adb2bb9e3ae6a725dd59d5f1da7b7c7db2928
Idan Richman <idan.richman@gmail.com>
2019-04-03 21:23:23 +0300

    add feature to resize images and jpg support instead of png (smaller, faster)

--- src/combine_images.py
+++ src/combine_images.py
@@ -5,7 +5,8 @@ import os
 from settings import settings
 from PIL import Image
 
-def append_images(images_fn, images_path, direction='horizontal', bg_color=(255,255,255), aligment='center'):
+
+def append_images(images_fn, images_path, direction='horizontal', bg_color=(255, 255, 255), aligment='center'):
     """
     Appends images in horizontal/vertical direction.
 
@@ -19,11 +20,11 @@ def append_images(images_fn, images_path, direction='horizontal', bg_color=(255,
     Returns:
         Concatenated image as a new PIL image object.
     """
-    images = [Image.open(os.path.join(images_path, img_fn)) for img_fn in images_fn] 
-    
+    images = [Image.open(os.path.join(images_path, img_fn)) for img_fn in images_fn]
+
     widths, heights = zip(*(i.size for i in images))
 
-    if direction=='horizontal':
+    if direction == 'horizontal':
         new_width = sum(widths)
         new_height = max(heights)
     else:
@@ -34,7 +35,7 @@ def append_images(images_fn, images_path, direction='horizontal', bg_color=(255,
 
     offset = 0
     for im in images:
-        if direction=='horizontal':
+        if direction == 'horizontal':
             y = 0
             if aligment == 'center':
                 y = int((new_height - im.size[1])/2)
@@ -53,6 +54,7 @@ def append_images(images_fn, images_path, direction='horizontal', bg_color=(255,
 
     return new_im
 
+
 def main():
     os.makedirs(settings['processed_pairs_folder'], exist_ok=True)
 
@@ -67,10 +69,13 @@ def main():
         for review in os.listdir(review_path):
             if review != 'product.jpg':
                 new_img = append_images(images_fn=['product.jpg', review], images_path=review_path)
+                if settings['resize_width'] != 0:  # 0 means no resizing
+                    new_img = new_img.resize((settings['resize_width'], int(settings['resize_width'] * new_img.height / new_img.width)))
                 review_i = re.sub("[^0-9]", "", review)  # remove all non-digits
-                new_img.save(os.path.join(settings['processed_pairs_folder'], '%s_%s.png' % (asin, review_i)))
+                new_img.save(os.path.join(settings['processed_pairs_folder'], '%s_%s.%s' % (asin, review_i, settings['processed_fileformat'])))
 
     print('\nDone.')
+
 
 if __name__ == '__main__':
     main()
